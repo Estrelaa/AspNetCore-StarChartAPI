6f85555116e91336a7697f6f0a2b81fde1147f2d
Kieran Winfield <K.Winfield@ucas.ac.uk>
2020-02-18 15:18:13 +0000

    Created get actions on controller

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Remotion.Linq.Clauses;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -12,6 +15,50 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext applicationDbContext)
         {
             _context = applicationDbContext;
+        }
+        [HttpGet("{id:int}", Name = "GetById")] 
+        public IActionResult GetById(int id)
+        {
+            var bodies = _context.CelestialObjects.Find(id);
+
+            if (bodies == null)
+            {
+                return NotFound();
+            }
+
+            bodies.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId == id).ToList();
+            return Ok(bodies);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var bodies = _context.CelestialObjects.Where(c => c.Name == name).ToList();
+            if (!bodies.Any())
+            {
+                return NotFound();
+            }
+
+            foreach (var celestialObject in bodies)
+            {
+                celestialObject.Satellites = _context.CelestialObjects
+                    .Where(c => c.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+            return Ok(bodies);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var bodies = _context.CelestialObjects.ToList();
+
+            foreach (var celestialObject in  bodies)
+            {
+                celestialObject.Satellites = _context.CelestialObjects
+                    .Where(c => c.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+
+            return Ok(bodies);
         }
     }
 }
